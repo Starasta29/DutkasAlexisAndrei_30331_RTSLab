f5f5223f7fe23097698bc8c4f66b60d76a682b80
Dutkas Alexis-Andrei <92668203+Starasta29@users.noreply.github.com>
2024-03-01 17:15:08 +0200

    Delete RTS directory

--- RTS/Lab2/src/main/java/org/example/Observable.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public interface Observable {
-    void addObserver(Observer observer);
-    void removeObserver(Observer observer);
-    void notifyObserver(int id, int value);
-}
